6ac7379f1b0b4623f9564a0c7c7b709272b5693f
Karzan Hassani <karz0009@edu.zealand.dk>
2023-05-16 12:22:21 +0200

    am done with index bike

--- EnterpriseCarDealership/Pages/CRUDKunder/Filters/IKundeFilters.cs
+++
@@ -1,9 +0,0 @@
-﻿using EnterpriseCarDealership.Models;
-
-namespace EnterpriseCarDealership.Pages.CRUDKunder.Filters
-{
-    public interface IKundeFilters
-    {
-        public List<Kunde> Filter();
-    }
-}
--- EnterpriseCarDealership/service repository's/repo/CarPaymentRepo.cs
+++ EnterpriseCarDealership/service repository's/repo/CarPaymentRepo.cs
@@ -2,18 +2,18 @@
 using EnterpriseCarDealership.Models;
 using EnterpriseCarDealership.service_repository_s.repo.interfaces;
 
-namespace EnterpriseCarDealership.service_repository_s.repo
-{
-    public class CarPaymentRepo : ICarPaymentRepo
-    {
-        private DealershipContext _db = new DealershipContext();
+//namespace EnterpriseCarDealership.service_repository_s.repo
+//{
+//    public class CarPaymentRepo : ICarPaymentRepo
+//    {
+//        private DealershipContext _db = new DealershipContext();
 
-        public CarPayment getCarPayment(int id)
-        {
-            throw new NotImplementedException();
-        }
+//        public CarPayment getCarPayment(int id)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-    }
+//    }
 
     
-}
+//}
